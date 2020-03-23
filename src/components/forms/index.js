d246aac6f1913739e63f40b8b158158bdc2d1831
kir4ik <vlados.linkoln@gmail.com>
2020-03-23 15:01:08 +0300

    setup modals

--- src/components/forms/index.js
+++
@@ -1,1 +0,0 @@
-export { default as CustomerForm } from './CustomerForm';
--- src/helpers/validators.js
+++ src/helpers/validators.js
@@ -1,14 +1,14 @@
-export const validateRequired = value => !(value && value.trim());
+export const validateRequired = value => !(value && String(value).trim());
 
-export const validateAlphabetic = value => value && !(/^[a-z]+$/i.test(value.trim()));
+export const validateAlphabetic = value => value && !(/^[a-z]+$/i.test(String(value).trim()));
 
 export const validateInteger = value => !Number.isInteger(+value);
 
-export const getValidatePhone = (min = 7, max = 15) => value => value && !(new RegExp(`^\\d{${min},${max}}$`).test(value.trim()));
+export const getValidatePhone = (min = 7, max = 15) => value => value && !(new RegExp(`^\\d{${min},${max}}$`).test(String(value).trim()));
 
-export const getValidateMinSize = min => value => value && value.trim().length < min;
+export const getValidateMinSize = min => value => value && String(value).trim().length < min;
 
-export const getValidateMaxSize = max => value => value && value.trim().length > max;
+export const getValidateMaxSize = max => value => value && String(value).trim().length > max;
 
 export const getValidateMinNumber = min => value => !Number.isFinite(+value) || +value < min;
 
